9a2c3c6463eaee739246c8d3fabfc8f6d628a74a
Nabeel Okal <nabeel.okal@gmail.com>
2025-04-23 01:13:05 +0300

    Building this game from scratch

--- main.cpp
+++ main.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
-#include <cstdlib>
 #include <vector>
 using namespace std;
 
 string selectRandWord();
+vector<char> fillTheBlanks(vector<char> text);
+void playerGuess(vector<char> wordTarget);
 
 int main() {
     // PLayer's lives
-    short lives = 10;
+    // short lives = 10;
+
     cout << "Welcome to Hangman Game!" << endl;
 
     string word = selectRandWord();
 
     // Converting from word into characters
+    vector<char> chosen_word(word.begin(), word.end());
 
-    vector<char> chosen_word {};
-    for (int i = 0; i < word.size(); i++) {
-        chosen_word.push_back(word[i]);
+    // Fill the blanks
+    vector<char> blanks = fillTheBlanks(chosen_word);
+    while (true) {
+        for (char blank: blanks)
+            cout << blank << " ";
+        cout << endl;
+        playerGuess(blanks);
     }
 
-    cout << "Chosen word: ";
-    for (char i: chosen_word) cout << i;
     return 0;
 }
 
 string selectRandWord() {
     srand(time(0));
     int r = rand() % 10;
-    vector<string> words {"Horizon", "Thunder", "Puzzle",
+    vector<string> words = {"Horizon", "Thunder", "Puzzle",
                             "Volcano", "Whisper", "Kingdom",
                             "Lantern", "Oxygen", "Freedom"};
     return words[r];
+}
+
+vector<char> fillTheBlanks(vector<char> text) {
+    int len = text.size();
+    vector<char> blanks {};
+    for (int i = 0; i < len; i++) {
+        blanks.push_back('_');
+    }
+    return blanks;
+}
+
+void playerGuess(vector<char> wordTarget) {
+    char guess {};
+    cout << "Enter your guess: ";
+    cin >> guess;
+
+    for (int i = 0; i < wordTarget.size(); i++) {
+        if (guess == wordTarget[i]) {
+            cout << "Correct!" << endl;
+            wordTarget.push_back(guess);
+        }else {
+            cout << "Incorrect!" << endl;
+        }
+    }
+
 }
